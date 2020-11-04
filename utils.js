cc6c0a0fec62800a5870ddb67826e2e700aeccc7
Joshua <tank158r5155@yahoo.com.tw>
2020-11-04 09:25:30 +0800

    Linting

--- utils.js
+++ utils.js
@@ -1,14 +1,15 @@
-const cp = require("child_process");
-const chalk = require("chalk");
-const axios = require("axios");
-const { getOr } = require("lodash/fp");
+const cp = require('child_process');
+const chalk = require('chalk');
+const axios = require('axios');
+const { getOr } = require('lodash/fp');
 
-const config = require("./config");
-require("dotenv").config({ path: config.envPath });
+const config = require('./config');
+require('dotenv').config({ path: config.envPath });
 
 const userName = process.env.USER_NAME;
 const masterIp = process.env.K8S_MASTER;
 const slaveIp = process.env.K8S_SLAVE;
+debugger;
 const API_ROOT = process.env.API_ROOT;
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -16,7 +17,7 @@ const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const run = (options = {}) => (command) => {
   const { isMaster = true, shouldPrint = true } = options;
   const serverIp = isMaster ? masterIp : slaveIp;
-  const stdio = shouldPrint ? { stdio: "inherit" } : {};
+  const stdio = shouldPrint ? { stdio: 'inherit' } : {};
 
   cp.execSync(`ssh ${userName}@${serverIp} ${command}`, stdio);
 };
@@ -24,7 +25,7 @@ const run = (options = {}) => (command) => {
 const getMode = async () => {
   try {
     const response = await axios.get(`${API_ROOT}/inspect/configurator`);
-    return getOr(null, "data.mode", response);
+    return getOr(null, 'data.mode', response);
   } catch (error) {
     console.log(
       chalk.red(`Oops, we cannot fetch Configurator info from ${API_ROOT}`)
